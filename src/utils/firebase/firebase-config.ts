1622cf71c71391ce482e82cc3e08ee9db09e10a5
Emre Can Erol <erolemrecan@outlook.com>
2023-03-13 02:26:44 +0300

    gitignore changes

---
+++ src/utils/firebase/firebase-config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCxslWa-qoo1fA9pUkD72fdrjcVbtT5wTo",
+  authDomain: "pinterestclone-f1558.firebaseapp.com",
+  projectId: "pinterestclone-f1558",
+  storageBucket: "pinterestclone-f1558.appspot.com",
+  messagingSenderId: "247251837693",
+  appId: "1:247251837693:web:973a5600203c07248c9494",
+  measurementId: "G-8ZJMEF125L",
+};
